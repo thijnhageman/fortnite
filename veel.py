02fb49ec183a7c1f2826bb0e99cc1e40958670b8
thijnhageman <thijnhageman@gmail.com>
2022-05-04 18:22:31 +0200

    nieuwe

--- veel.py
+++ veel.py
@@ -1,7 +1,9 @@
 
-
+out = open("nummers.txt", "w")
 mssg = "![{}](pics/{}.jpg)"
 
-for i in range(131, 250):
-    print(mssg.format(i, i))
-    print()
+for i in range(251, 1000):
+    print(mssg.format(i, i), file=out)
+    print(file=out)
+out.close()
+print("klaar")
